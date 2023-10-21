7f36e4cdaad73d22dcaaa9164e4dcb1585bb9369
Vincent Dais <vincentpaul.dais@nbscollege.edu.ph>
2023-10-21 16:19:59 +0800

    Re-commit

--- app/src/main/java/com/nbscollege/professorsattendanceapp/AuthenticationScreens/LoadingScreen.kt
+++ app/src/main/java/com/nbscollege/professorsattendanceapp/AuthenticationScreens/LoadingScreen.kt
@@ -15,8 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.nbscollege.professorsattendanceapp.R
+  import com.nbscollege.professorsattendanceapp.R
 import kotlinx.coroutines.delay
 
 
